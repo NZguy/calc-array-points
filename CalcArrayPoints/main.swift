77dc172f6116018749e511f1d92db7da23a82ca9
Duncan Andrew <duncanandrew@live.com>
2017-04-04 21:26:05 -0700

    Initial Commit

---
+++ CalcArrayPoints/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CalcArrayPoints
+//
+//  Created by Duncan Andrew on 4/4/17.
+//  Copyright © 2017 Duncan Andrew. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
